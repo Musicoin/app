37038ba1c8c55f9057c6df2212f215ac6d80269f
bengyles <bengyles@hotmail.com>
2018-11-07 21:30:26 +0100

    Added some more error handling on api requests and upgraded to newer Expo SDK

--- actions/index.js
+++ actions/index.js
@@ -93,14 +93,21 @@ export function fetchReleases() {
 }
 
 async function fetchTrackImageJson(imageId) {
-  return fetch(`https://musicoin.org/i2i/${imageId}`, {
-    method: 'GET',
-    headers: {
-      'cache-control': 'no-cache',
-    },
-  }).then(response => {
-    return response.json();
-  });
+  try {
+    return fetch(`https://musicoin.org/i2i/${imageId}`, {
+      method: 'GET',
+      headers: {
+        'cache-control': 'no-cache',
+      },
+    }).then(response => {
+      return response.json();
+    }).catch(e => {
+      console.log(e);
+      return 'https://i.redd.it/y2hj9ovrrne11.jpg';
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 function addTip(trackId, json) {
@@ -161,50 +168,65 @@ export function deleteAlert() {
 }
 
 async function fetchGetData(action, params) {
-  let newParams = {
-    ...params,
-    'email': API_EMAIL,
-  };
+  try {
+    let newParams = {
+      ...params,
+      'email': API_EMAIL,
+    };
 
-  let formBody = [];
-  for (let property in newParams) {
-    let encodedKey = encodeURIComponent(property);
-    let encodedValue = encodeURIComponent(newParams[property]);
-    formBody.push(encodedKey + '=' + encodedValue);
+    let formBody = [];
+    for (let property in newParams) {
+      let encodedKey = encodeURIComponent(property);
+      let encodedValue = encodeURIComponent(newParams[property]);
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+    formBody = formBody.join('&');
+
+    return fetch(API_ENDPOINT + action + formBody, {
+      method: 'GET',
+      headers: {
+        'cache-control': 'no-cache',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    }).then(response => {
+      return response.json();
+    }).catch(e => {
+      console.log(e);
+    });
+  } catch (e) {
+    console.log(e);
   }
-  formBody = formBody.join('&');
 
-  return fetch(API_ENDPOINT + action + formBody, {
-    method: 'GET',
-    headers: {
-      'cache-control': 'no-cache',
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  }).then(response => {
-    return response.json();
-  }).catch(e => console.log(e));
 }
 
 async function fetchPostData(action, params) {
-  let newParams = {
-    ...params,
-    'email': API_EMAIL,
-  };
+  try {
+    let newParams = {
+      ...params,
+      'email': API_EMAIL,
+    };
 
-  let formBody = [];
-  for (let property in newParams) {
-    let encodedKey = encodeURIComponent(property);
-    let encodedValue = encodeURIComponent(newParams[property]);
-    formBody.push(encodedKey + '=' + encodedValue);
+    let formBody = [];
+    for (let property in newParams) {
+      let encodedKey = encodeURIComponent(property);
+      let encodedValue = encodeURIComponent(newParams[property]);
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+    formBody = formBody.join('&');
+
+    return fetch(API_ENDPOINT + action, {
+      method: 'POST',
+      headers: {
+        'cache-control': 'no-cache',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: formBody,
+    }).then(response => {
+      response.json();
+    }).catch(e => {
+      console.log(e);
+    });
+  } catch (e) {
+    console.log(e);
   }
-  formBody = formBody.join('&');
-
-  return fetch(API_ENDPOINT + action, {
-    method: 'POST',
-    headers: {
-      'cache-control': 'no-cache',
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: formBody,
-  }).then(response => response.json());
 }
