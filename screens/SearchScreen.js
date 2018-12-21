91982302359fbe332fa1bda5b8e22f0dfb9566ed
bengyles <bengyles@hotmail.com>
2018-12-21 19:13:19 +0100

    Added tab view in search results

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Text, StyleSheet, StatusBar, Platform, FlatList, RefreshControl, SectionList, TouchableOpacity} from 'react-native';
+import {TabView, TabBar, SceneMap} from 'react-native-tab-view';
 import Colors from '../constants/Colors';
 import {SearchBar} from 'react-native-elements';
 import GenreList from '../components/GenreList';
@@ -11,13 +12,131 @@ import Track from '../components/track/track';
 import Artist from '../components/Artist';
 
 let numArtists = 3;
-let numTracks = 4;
+let numTracks = 3;
 
 class SearchScreen extends React.Component {
+
   constructor(props) {
     super(props);
-    this.state = {searching: false, artistsExpanded: false, releasesExpanded: false};
+    this.state = {
+      searching: false,
+      index: 0,
+      route: 'all',
+      routes: [
+        {key: 'all', title: 'ALL'},
+        {key: 'artists', title: 'ARTISTS'},
+        {key: 'tracks', title: 'TRACKS'},
+      ],
+    };
   }
+
+  _renderAllResults = () => (
+      <SectionList
+          initialNumToRender={5}
+          keyExtractor={this._keyExtractor}
+          stickySectionHeadersEnabled={false}
+          style={{flex: 1, marginBottom: this.props.currentTrack ? Layout.playerHeight : 0}} contentContainerStyle={styles.contentContainer}
+          renderSectionHeader={({section}) => {
+            if (!this.props.loading.SEARCH) {
+              if ((section.title === 'Artists' && this.props.searchResults.artists.length > 0) || (section.title === 'Tracks' && this.props.searchResults.releases.length > 0)) {
+                return (
+                    <View style={{marginHorizontal: 16, marginTop: 40, marginBottom: 16}}>
+                      <Text style={{fontSize: 16, color: '#F4F7FB'}}>{section.title}</Text>
+                    </View>
+                );
+              }
+            }
+          }}
+          renderSectionFooter={({section}) => {
+            if (!this.props.loading.SEARCH) {
+              if (section.title === 'Artists') {
+                if (this.props.searchResults.artists.length > numArtists) {
+                  return (
+                      <View style={{marginHorizontal: 16, height: 32, alignItems: 'center', justifyContent: 'center'}}>
+                        <TouchableOpacity onPress={() => this.seeMore(section.title)}>
+                          <Text style={{fontSize: 12, color: '#707070'}}>{'see more artists'}</Text>
+                        </TouchableOpacity>
+                      </View>
+                  );
+                }
+              }
+              if (section.title === 'Tracks') {
+                if (this.props.searchResults.releases.length > numTracks) {
+                  return (
+                      <View style={{marginHorizontal: 16, height: 32, alignItems: 'center', justifyContent: 'center'}}>
+                        <TouchableOpacity onPress={() => this.seeMore(section.title)}>
+                          <Text style={{fontSize: 12, color: '#707070'}}>{'see more tracks'}</Text>
+                        </TouchableOpacity>
+                      </View>
+                  );
+                }
+
+                if (this.props.searchResults.releases.length == 0 && this.props.searchResults.artists.length == 0) {
+                  return (
+                      <View style={{height: 100, alignItems: 'center', justifyContent: 'center'}}>
+                        <Text style={{color: Colors.tabIconDefault}}>Seems like you're played the wrong note!</Text>
+                      </View>
+                  );
+                }
+              }
+            }
+          }}
+          sections={[
+            {
+              title: 'Artists', data: this.props.searchResults.artists.slice(0, numArtists),
+              renderItem: this._renderArtistItem,
+            },
+            {
+              title: 'Tracks', data: this.props.searchResults.releases.slice(0, numTracks),
+              renderItem: this._renderItem,
+            },
+          ]}
+          refreshControl={
+            <RefreshControl
+                refreshing={this.props.loading.SEARCH}
+                onRefresh={() => this.search()}
+                tintColor={Colors.tintColor}
+            />}
+      />
+  );
+
+  _renderArtists = () => (
+      <FlatList
+          data={this.props.searchResults.artists}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderArtistItem}
+          style={{flex: 1, marginBottom: this.props.currentTrack ? Layout.playerHeight : 0}} contentContainerStyle={styles.contentContainer}
+          refreshControl={
+            <RefreshControl
+                refreshing={this.props.loading.SEARCH}
+                onRefresh={() => this.search()}
+                tintColor={Colors.tintColor}
+            />
+          }
+          ListEmptyComponent={!this.props.loading.SEARCH ? <View style={{height: 100, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{color: Colors.tabIconDefault}}>No artists found</Text>
+          </View> : null}
+      />
+  );
+
+  _renderTracks = () => (
+      <FlatList
+          data={this.props.searchResults.releases}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+          style={{flex: 1, marginBottom: this.props.currentTrack ? Layout.playerHeight : 0}} contentContainerStyle={styles.contentContainer}
+          refreshControl={
+            <RefreshControl
+                refreshing={this.props.loading.SEARCH}
+                onRefresh={() => this.search()}
+                tintColor={Colors.tintColor}
+            />
+          }
+          ListEmptyComponent={!this.props.loading.SEARCH ? <View style={{height: 100, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{color: Colors.tabIconDefault}}>No tracks found</Text>
+          </View> : null}
+      />
+  );
 
   render() {
     return (
@@ -48,75 +167,16 @@ class SearchScreen extends React.Component {
               }}
           />
           {this.state.searching ?
-              <SectionList
-                  initialNumToRender={5}
-                  keyExtractor={this._keyExtractor}
-                  stickySectionHeadersEnabled={false}
-                  style={{flex: 1, marginBottom: this.props.currentTrack ? Layout.playerHeight : 0}} contentContainerStyle={styles.contentContainer}
-                  renderSectionHeader={({section}) => {
-                    if (!this.props.loading.SEARCH) {
-                      if ((section.title === 'Artists' && this.props.searchResults.artists.length > 0) || (section.title === 'Tracks' && this.props.searchResults.releases.length > 0)) {
-                        return (
-                            <View style={{marginHorizontal: 16, marginTop: 40, marginBottom: 16}}>
-                              <Text style={{fontSize: 16, color: '#F4F7FB'}}>{section.title}</Text>
-                            </View>
-                        );
-                      }
-                    }
-                  }}
-                  renderSectionFooter={({section}) => {
-                    if (!this.props.loading.SEARCH) {
-                      if (section.title === 'Artists') {
-                        if (this.props.searchResults.artists.length > numArtists) {
-                          return (
-                              <View style={{marginHorizontal: 16, height: 32, alignItems: 'center', justifyContent: 'center'}}>
-                                <TouchableOpacity onPress={() => this.toggleMore(section.title)}>
-                                  <Text style={{fontSize: 12, color: '#707070'}}>{this.state.artistsExpanded ? 'see less artists' : 'see more artists'}</Text>
-                                </TouchableOpacity>
-                              </View>
-                          );
-                        }
-                      }
-                      if (section.title === 'Tracks') {
-                        if (this.props.searchResults.releases.length > numTracks) {
-                          return (
-                              <View style={{marginHorizontal: 16, height: 32, alignItems: 'center', justifyContent: 'center'}}>
-                                <TouchableOpacity onPress={() => this.toggleMore(section.title)}>
-                                  <Text style={{fontSize: 12, color: '#707070'}}>{this.state.releasesExpanded ? 'see less tracks' : 'see more tracks'}</Text>
-                                </TouchableOpacity>
-                              </View>
-                          );
-                        }
-
-                        if (this.props.searchResults.releases.length == 0 && this.props.searchResults.artists.length == 0) {
-                          return (
-                              <View style={{height: 100, alignItems: 'center', justifyContent: 'center'}}>
-                                <Text style={{color: Colors.tabIconDefault}}>No results</Text>
-                              </View>
-                          );
-                        }
-                      }
-                    }
-                  }}
-                  sections={[
-                    {
-                      title: 'Artists', data: this.state.artistsExpanded ? this.props.searchResults.artists : this.props.searchResults.artists.slice(0, numArtists),
-                      renderItem: this._renderArtistItem,
-                    },
-                    {
-                      title: 'Tracks', data: this.state.releasesExpanded ? this.props.searchResults.releases : this.props.searchResults.releases.slice(0, numTracks),
-                      renderItem: this._renderItem,
-                    },
-                  ]}
-                  refreshControl={
-                    <RefreshControl
-                        refreshing={this.props.loading.SEARCH}
-                        onRefresh={() => this.search()}
-                        tintColor={Colors.tintColor}
-                    />}
-                  ListEmptyComponent={!this.props.loading.SEARCH ? <View style={{height: 100, alignItems: 'center', justifyContent: 'center'}}>
-                    <Text style={{color: Colors.tabIconDefault}}>Seems like you played the wrong note!</Text>
-                  </View> : null}
+              <TabView
+                  navigationState={this.state}
+                  renderScene={SceneMap({
+                    all: this._renderAllResults,
+                    artists: this._renderArtists,
+                    tracks: this._renderTracks,
+                  })}
+                  renderTabBar={this._renderTabBar}
+                  onIndexChange={index => this.setState({index, route: this.state.routes[index].key})}
+                  initialLayout={{width: Layout.window.width, height: Layout.window.height}}
               />
               :
               <GenreList/>
@@ -124,6 +184,23 @@ class SearchScreen extends React.Component {
         </View>
     );
   }
+
+  _renderTabBar = props => {
+    return (
+        <TabBar
+            {...props}
+            indicatorStyle={{backgroundColor: Colors.tintColor, color: Colors.tintColor}}
+            style={{backgroundColor: Colors.backgroundColor}}
+            renderLabel={this._renderLabel}
+        />
+    );
+  };
+
+  _renderLabel = scene => {
+    console.log(scene);
+    const label = scene.route.title;
+    return <Text style={{color: scene.route.key === this.state.route ? Colors.tintColor : Colors.tabIconDefault, fontSize: 14}}>{label}</Text>;
+  };
 
   componentWillUnmount() {
     clearTimeout(this.timeout);
@@ -152,13 +229,13 @@ class SearchScreen extends React.Component {
       <Artist artist={item}/>
   );
 
-  toggleMore(sectionTitle) {
+  seeMore(sectionTitle) {
     if (sectionTitle === 'Artists') {
-      this.setState({artistsExpanded: !this.state.artistsExpanded});
+      this.setState({index: 1, route: this.state.routes[1].key});
     }
 
     if (sectionTitle === 'Tracks') {
-      this.setState({releasesExpanded: !this.state.releasesExpanded});
+      this.setState({index: 2, route: this.state.routes[2].key});
     }
   }
 
