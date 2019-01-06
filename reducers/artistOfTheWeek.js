427c237d785aa8c54b2bca570f85be61407eab2a
bengyles <bengyles@hotmail.com>
2019-01-06 04:29:57 +0100

    Changed Artist Of The Week data because of api change

--- reducers/artistOfTheWeek.js
+++ reducers/artistOfTheWeek.js
@@ -3,23 +3,7 @@ import {ARTIST_OF_THE_WEEK_SUCCESS} from '../constants/Actions';
 export default function artistOfTheWeek(state = null, action) {
   switch (action.type) {
     case ARTIST_OF_THE_WEEK_SUCCESS:
-      if (action.data) {
-        let track = {
-          title: action.data.trackName,
-          trackImg: action.data.trackImg,
-          trackAddress: action.data.trackAddress,
-        };
-
-        let artist = {
-          artistName: action.data.artistName,
-          imageUrl: action.data.artistImg,
-          profileAddress: action.data.artistAddress,
-        };
-
-        return {track, artist};
-      } else {
-        return state;
-      }
+      return action.data.track && action.data.artist ? action.data : state;
     default:
       return state;
   }
