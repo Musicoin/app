dab3020364304c95d47040f540d46582e40c237f
bengyles <bengyles@hotmail.com>
2019-01-07 11:11:06 +0100

    Refresh the track object before playing since trackUrl could be outdated

--- actions/player.js
+++ actions/player.js
@@ -1,9 +1,40 @@
-import {PLAY_TRACK} from '../constants/Actions';
+import {PLAY_TRACK, RECEIVE_NEW_RELEASES_REQUEST} from '../constants/Actions';
+import {fetchGetData} from '../tools/util';
+import {API_VERSION} from 'react-native-dotenv';
 
-export function playTrack(track, addToLastPlayed = true) {
+export function dispatchPlayTrack(track, addToLastPlayed = true) {
   return {
     type: PLAY_TRACK,
     track,
-    addToLastPlayed
+    addToLastPlayed,
   };
 }
+
+async function fetchReleaseDetailsJson(token, email, track) {
+  var params = {
+    'accessToken': token,
+    'email': email,
+  };
+
+  let result = await fetchGetData(`release/detail/${API_VERSION}/${track.trackAddress}?`, params);
+
+  if (result.success && result.data != null) {
+    result.origin = track.origin;
+    return result;
+  } else {
+    return false;
+  }
+}
+
+export function playTrack(track, addToLastPlayed) {
+  return function(dispatch, getState) {
+    let {accessToken, email} = getState().auth;
+    return fetchReleaseDetailsJson(accessToken, email, track).then(json => {
+      if (json.success) {
+        dispatch(dispatchPlayTrack(json.data, addToLastPlayed));
+      } else {
+        dispatch(dispatchPlayTrack(track, addToLastPlayed));
+      }
+    });
+  };
+};
--- reducers/currentTrack.js
+++ reducers/currentTrack.js
@@ -3,7 +3,7 @@ import {PLAY_TRACK} from '../constants/Actions';
 export default function currentTrack(state = null, action) {
   switch (action.type) {
     case PLAY_TRACK:
-      return action.track;
+      return action.track ? action.track : state;
     default:
       return state;
   }
