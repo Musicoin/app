f7ec6d6b1fd1d32510643cf263cf2a51ddeb4fa5
bengyles <bengyles@hotmail.com>
2019-01-07 10:33:03 +0100

    Implemented new tipping api

--- actions/tip.js
+++ actions/tip.js
@@ -2,13 +2,14 @@ import {TIP_TRACK, ALLOW_NEXT_TIP} from '../constants/Actions';
 import {TIP_TIMEOUT_MILIS} from '../constants/App';
 import {fetchPostFormDataJson} from '../tools/util';
 import {addAlert} from './alert';
+import {API_VERSION} from 'react-native-dotenv';
 
 function addTip(trackAddress, json) {
   console.log(json);
   return function(dispatch, getState) {
     dispatch({type: ALLOW_NEXT_TIP, data: false});
     let success = false;
-    if (json.success) {
+    if (json.tx) {
       dispatch(addAlert('success', 'Your tip has been sent successfully!', 'Thanks for supporting your favorite artists.'));
       setTimeout(() => dispatch({type: ALLOW_NEXT_TIP, data: true}), TIP_TIMEOUT_MILIS);
       success = true;
@@ -26,10 +27,11 @@ function addTip(trackAddress, json) {
 
 async function tipTrackJson(trackAddress, token, email) {
   let params = {
-    tip: 1,
+    trackAddress,
+    musicoins: 1,
   };
 
-  let tipTrack = await fetchPostFormDataJson(`release/tip/${trackAddress}?email=${email}&accessToken=${token}`, params);
+  let tipTrack = await fetchPostFormDataJson(`release/tiptrack/${API_VERSION}?email=${email}&accessToken=${token}`, params);
   return tipTrack;
 }
 
