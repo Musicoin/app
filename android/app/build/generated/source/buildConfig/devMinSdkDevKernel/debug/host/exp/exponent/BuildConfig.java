33cafb2855997381d9429146de785ceb1ecdc406
bengyles <bengyles@hotmail.com>
2019-02-12 13:15:26 +0100

    Updated version and built new apk/ipa files to support v2 and release channel

--- android/app/build/generated/source/buildConfig/devMinSdkDevKernel/debug/host/exp/exponent/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/devMinSdkDevKernel/debug/host/exp/exponent/BuildConfig.java
@@ -8,8 +8,8 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "org.musicoin.musicoin";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "devMinSdkDevKernel";
-  public static final int VERSION_CODE = 5;
-  public static final String VERSION_NAME = "1.1.0";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "2.0.0";
   public static final String FLAVOR_minSdk = "devMinSdk";
   public static final String FLAVOR_remoteKernel = "devKernel";
 }
