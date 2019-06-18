84e31e43182569767c5a9b66adcfd1d78345c328
bengyles <bengyles@hotmail.com>
2019-06-18 14:01:44 +0200

    Removed more files specified by gitignore and changed path for android local development

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "musicoin://192.168.2.104:80";
+  public static final String DEVELOPMENT_URL = "musicoin://192.168.2.104:19000";
 
 }
