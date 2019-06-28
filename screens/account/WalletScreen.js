11ffcf25190a58b6f35b5ec692518096ee63d0da
bengyles <bengyles@hotmail.com>
2019-06-28 13:44:11 +0200

    issue #12 Added link to explorer when tapping on the balance

--- screens/account/WalletScreen.js
+++ screens/account/WalletScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, Platform, Dimensions, Clipboard} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, Platform, Dimensions, Clipboard, Linking} from 'react-native';
 import Colors from '../../constants/Colors';
 import * as Icon from '@expo/vector-icons';
 import {getStatusBarHeight} from 'react-native-iphone-x-helper/index';
@@ -39,47 +39,61 @@ class WalletScreen extends React.Component {
 
             <View style={{backgroundColor: '#0B0C0D', marginTop: 8, paddingVertical: 24}}>
               <Text style={{color: Colors.tintColor, fontSize: 16, textAlign: 'center'}}>$MUSIC</Text>
-              <Text style={{color: Colors.fontColor, fontSize: 24, textAlign: 'center', marginVertical: 16}}>{parseFloat(this.props.profile.balance).toFixed(2)}</Text>
-            </View>
+              <TouchableOpacity onPress={() => {
+                let url = `https://explorer.musicoin.org/account/${this.props.profile.profileAddress}`;
+                Linking.canOpenURL(url).then((supported) => {
+                  if (!supported) {
+                    this.props.addAlert('error', '', 'Can\'t open the explorer right now, please try again at a later time.');
+                  } else {
+                    return Linking.openURL(url);
+                  }
+                }).catch((err) => {
+                  console.error('An error occurred', err);
+                  this.props.addAlert('error', '', 'Can\'t open the explorer right now, please try again at a later time.');
+                });
+              }}>
+                <Text style={{color: Colors.fontColor, fontSize: 24, textAlign: 'center', marginVertical: 16}}>{parseFloat(this.props.profile.balance).toFixed(2)}</Text>
+                </TouchableOpacity>
+                </View>
 
-            {this.props.profile.profileAddress ?
+                {this.props.profile.profileAddress ?
                 <View>
-                  <Text style={{color: Colors.disabled, fontSize: 14, textAlign: 'center', marginTop: 16}}>Your wallet address</Text>
+                <Text style={{color: Colors.disabled, fontSize: 14, textAlign: 'center', marginTop: 16}}>Your wallet address</Text>
 
-                  <TouchableOpacity
-                      onPress={() => {
-                        Clipboard.setString(this.props.profile.profileAddress);
-                        this.props.addAlert('info', '', 'Wallet address copied!');
-                      }}
-                      style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: '#0B0C0D', marginTop: 16, padding: 16}}>
-                    <Text style={{color: Colors.tintColor, fontSize: 10, textAlign: 'center'}}>{this.props.profile.profileAddress}</Text>
-                    <View style={{justifyContent: 'center', marginHorizontal: 8}}>
-                      <Icon.Ionicons
-                          name={Platform.OS === 'ios' ? `md-albums` : 'md-albums'}
-                          size={12}
-                          color={Colors.fontColor}
-                      />
-                    </View>
-                  </TouchableOpacity>
+                <TouchableOpacity
+                onPress={() => {
+                Clipboard.setString(this.props.profile.profileAddress);
+                this.props.addAlert('info', '', 'Wallet address copied!');
+                }}
+                style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: '#0B0C0D', marginTop: 16, padding: 16}}>
+                <Text style={{color: Colors.tintColor, fontSize: 10, textAlign: 'center'}}>{this.props.profile.profileAddress}</Text>
+                <View style={{justifyContent: 'center', marginHorizontal: 8}}>
+                <Icon.Ionicons
+                name={Platform.OS === 'ios' ? `md-albums` : 'md-albums'}
+                size={12}
+                color={Colors.fontColor}
+                />
+                </View>
+                </TouchableOpacity>
                 </View> :
                 <Text style={{color: Colors.errorText, fontSize: 14, textAlign: 'center', marginTop: 16}}>No wallet address</Text>
-            }
-          </View>
-        </View>
-    );
-  }
-}
+                }
+                </View>
+                </View>
+                );
+                }
+                }
 
-function mapStateToProps(state) {
-  return state;
-}
+                function mapStateToProps(state) {
+                return state;
+                }
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: getStatusBarHeight(true),
-    flex: 1,
-    backgroundColor: Colors.backgroundColor,
-  },
-});
+                const styles = StyleSheet.create({
+                container: {
+                paddingTop: getStatusBarHeight(true),
+                flex: 1,
+                backgroundColor: Colors.backgroundColor,
+                },
+                });
 
-export default connect(mapStateToProps, {addAlert})(WalletScreen);
+                export default connect(mapStateToProps, {addAlert})(WalletScreen);
